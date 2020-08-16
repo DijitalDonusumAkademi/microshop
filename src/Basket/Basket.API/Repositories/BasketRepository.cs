c669804947971c25000624da589b56ef20df1308
Jerry Sun <jerysun007@hotmail.com>
2020-08-17 01:50:32 +0200

    added project Basket.API

---
+++ src/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,27 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task<bool> DeleteBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketCart> GetBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketCart> UpdateBasket(BasketCart basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
